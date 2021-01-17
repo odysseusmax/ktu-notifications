38a278c67fd7508a108f758bdc08ed642040b8b2
odysseusmax <royschristy@gmail.com>
2021-01-17 12:11:16 +0530

    fix typo

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from pathlib import Path
 class Config:
     NOTIFICATIONS_FILE = Path("notifications.json")
 
-    PORT = os.environ.get("HOST", "3141")
+    PORT = os.environ.get("PORT", "3141")
     HOST = os.environ.get("HOST", "0.0.0.0")
 
     DEBUG = bool(os.environ.get("DEBUG"))
